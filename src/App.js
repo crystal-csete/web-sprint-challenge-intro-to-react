ea87ee3a650f7c3fd86eb7e1dc146d3e06f5029a
crystal-csete <68755171+crystal-csete@users.noreply.github.com>
2020-10-09 11:37:19 -0600

    added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
-const App = () => {
+
+
+function App()  {
+  const [characters, setCharacters] = useState('')
+
+useEffect(() => {
+  
+  fetch("https://rickandmortyapi.com/api/character/1,2,3,4,5")
+    .then(response => response.data)
+    .then(myCharacters => {
+      console.log(myCharacters);
+      setMyCharacters(myCharacters)
+    });
+    
+}, [])
+
+
+
+
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
@@ -12,6 +29,9 @@ const App = () => {
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+          <button onClick={() => (myCharacters)}>
+    See details here!
+    </button>
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,17 @@
 // Write your Character component here
+import React from "react";
+
+
+const Character = props => {
+    const { character } = props;
+
+    return (
+        <div className='character-text'>
+            <span className='character'>{character.name}</span>
+            {' '}
+            <span className='location'>{character.location}</span>
+        </div>
+    );
+};
+
+export default Character;
